2701c6523dc4c28af280103e879406067e0f2cf9
Joseph DeYoung <jldeyoung01@bohe27ab3x.admin.bvsd.k12.co.us>
2017-05-16 14:28:13 -0600

    Added ankle pants font for labels, integrated on main menu

--- Phi Squad Game/MainMenuScene.swift
+++ Phi Squad Game/MainMenuScene.swift
@@ -13,18 +13,20 @@ class MainMenuScene: SKScene{
     override func didMove(to view: SKView) {
         var background: SKSpriteNode
         background = SKSpriteNode(imageNamed: "SplashScreen.png")
+        background.setScale(0.75)
         background.zPosition = 5
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         self.addChild(background)
         
-        let pressAnywhere = SKLabelNode(fontNamed: "chalkduster")
+        let pressAnywhere = SKLabelNode(fontNamed: "Anklepants")
+        pressAnywhere.removeFromParent()
         pressAnywhere.text = "tap anywhere to begin"
-        pressAnywhere.color = SKColor.blue
-        pressAnywhere.fontSize = 100
+        pressAnywhere.color = UIColor.blue
+        pressAnywhere.fontSize = 80
         pressAnywhere.zPosition = 200
         pressAnywhere.horizontalAlignmentMode = .center
         pressAnywhere.verticalAlignmentMode = .center
-        pressAnywhere.position = CGPoint(x: 0,y: 0)
+        pressAnywhere.position = CGPoint(x: 1334/2,y: 750/2+135)
         scene?.addChild(pressAnywhere)
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
